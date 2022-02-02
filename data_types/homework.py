8f2878f5ba25aed678ea36897362a00f589247c3
Petar Netev <pesho.netev@gmail.com>
2022-02-02 22:23:10 +0200

    Add data_types homework

---
+++ data_types/homework.py
@@ -0,0 +1,88 @@
+def print_student_sentence(name, age, today):
+    """
+    :param name:
+    :param age:
+    :param today:
+    :return: None
+    """
+    # Write code that prints out a sentence like
+    # <name> is aged <age>. He is a passionate learner, and he started a JD program just <today>
+    pass
+
+
+transactions_even = []
+
+
+def store_transactions(transaction_id, transactions):
+    """
+    :param transaction_id:
+    :param transactions:
+    :return: transactions
+    """
+    # Write code that stores the transaction_id if it is an even number
+    # The function should return the transactions
+    pass
+
+
+def pretty_print_dict(dictionary):
+    """
+    :param dictionary:
+    :return: NOne
+
+    If we have the following dictionary
+    {
+        level1: {
+            attr1: value,
+            attr2: value,
+            level2: {
+                attr1: value,
+                attr2: value,
+                level3: {
+                    attr1: value,
+                }
+            }
+        }
+    }
+    The desired output is the dictionary data printed in a pretty manner - tabulated (4 tabs) per
+    level
+    """
+    pass
+
+
+sums_array = []
+
+
+def store_sum(number1, number2, sums):
+    """
+    :param number1:
+    :param number2:
+    :param sums:
+    :return: sums
+    """
+    # Write a code that stores the sum of number1 and number2 within sums_array
+    pass
+
+
+def get_chars_count(string):
+    """
+    :param string:
+    :return: dict holding string chars count
+    """
+    pass
+
+
+def get_abbreviation(string):
+    """
+    :param string:
+    :return: abbreviation from the string (first 3 symbols). If string is shorter than 5 chars,
+    return None
+    """
+    pass
+
+
+def get_titled_string(sentence):
+    """
+    :param sentence:
+    :return: Return a titled version of the sentence
+    """
+    pass

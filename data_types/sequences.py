e65c1be5ddfb219b7c2e2f081a33e45f94952484
Petar Netev <pesho.netev@gmail.com>
2022-02-02 12:43:51 +0200

    Add more examples on data types

--- data_types/sequences.py
+++ data_types/sequences.py
@@ -13,11 +13,28 @@ print(name.split())
 
 
 # formatted string
-
 sentence = '{name} is {aged} and lives in {living_place}.'
 print(sentence.format(name='Pesho', aged=28, living_place='Sofia'))
 # >>> Pesho is 28 and lives in Sofia.
 
 
 # define a simple ages list variable
-ages_list = [14, 15, 14, 15, 16, 17, 8, 19, 18]
+ages_list = [14, 15, 14, 15, 16, 17, 8, 19, 18, 35]
+
+# add new age to ages_list
+
+# remove age from ages_list
+ages_list.remove(14)
+
+# iterate over list
+for age in ages_list:
+    print(age)
+
+
+coords = (45.2545, 26.5874)
+coords += (487.212, 222.222)
+print(coords)  # The result is new tuple coords created, not just mutated the original
+
+for coord in coords:
+    print(coord)
+

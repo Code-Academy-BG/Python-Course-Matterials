ab9373ce6f54ef7ec5fca3117d2a19442e2305f5
Petar Netev <pesho.netev@gmail.com>
2022-02-07 22:28:04 +0200

    Add materials to functions_and_generators topic

--- functions_and_generators/defs.py
+++ functions_and_generators/defs.py
@@ -38,3 +38,44 @@ users_list(
     main_user_age=25,
     **{f'User{x}': (x+1)*19 for x in range(19)},
 )
+
+
+# Example of using mutable data types as function parameters
+def update_ages(age, passed, future):
+    passed.append(age)
+    age += 1
+    future = [age + x for x in range(1, 4)]
+    print(age, future)
+
+
+mine_age = 31
+passed_ages = [_ for _ in range(1, mine_age)]
+next_three_ages = [mine_age + _ for _ in range(1, 4)]
+
+update_ages(mine_age, passed_ages, next_three_ages)
+print('After update_ages executed')
+print(mine_age, passed_ages, next_three_ages)
+
+
+age_module_scope = 31
+print('Module scope: ', age_module_scope)
+
+
+def scope_test():
+    var_one = 5
+    var_two = 6
+    ages = [25]
+
+    def scope_inner_test():
+        global age_module_scope
+        age_module_scope += 5
+        var_one = 6
+        var_two = 7
+        ages.append(26)
+        print('Inner scope: ', var_one, var_two, ages, age_module_scope)
+
+    scope_inner_test()
+    print('Outer scope: ', var_one, var_two, ages, age_module_scope)
+
+
+scope_test()

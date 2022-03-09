62737b19e97fc4c2b5e821b630b153e0401fe5d9
Petar Netev <pesho.netev@gmail.com>
2022-03-09 22:19:49 +0200

    Add generators and decorators materials

--- functions/decorators.py
+++ functions/decorators.py
@@ -1,20 +1,44 @@
+from functools import wraps
+
+
 def pre_tag(func):
+    # @wraps(func)
     def wrapper_func(*args):
-        print(f'<pre>{func(*args)}</pre>')
+        func_result = func(*args)
+        return f'<pre>{func_result}</pre>'
+    return wrapper_func
+
+
+def bold(func):
+    # @wraps(func)
+    def wrapper_func(*args):
+        func_result = func(*args)
+        return f'<b>{func_result}</b>'
     return wrapper_func
 
 
 @pre_tag
+@bold
 def print_name(name):
+    """print_name decorated function docstring"""
     return name
 
 
-print_name('Pesho')
+"""
+Checkout what print_name.__doc__ before and after applying @wraps(func) on the decorator 
+functions.
+
+The same is valid for decorated_print_name as well.
+"""
+
+# print_name('Pesho')
+decorated_print_name = pre_tag(bold(print_name))
 
 
 def encode_id(func):
+    @wraps(func)
     def wrapper_func(user_id):
-        return f'encoded_{func(user_id)}_user_id'
+        return f'encoded_{func(user_id) * 1200}_user_id'
     return wrapper_func
 
 
--- functions/generators.py
+++ functions/generators.py
@@ -1,11 +1,67 @@
+class Repeater:
+    def __init__(self, value):
+        self.value = value
+        self.start = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start < 10:
+            self.start += 1
+            return self.value
+        raise StopIteration("Endless iterator is dangerous")
+
+
+repeater = Repeater("hello")
+
+for x in repeater:
+    print(x)
+
+
+class FirstN:
+    def __init__(self, n):
+        self.n = n
+        self.num = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.num < self.n:
+            current, self.num = self.num, self.num + 1
+            return current
+        raise StopIteration()
+
+
+print(sum(FirstN(505)))
+
+
+def first_n(n):
+    num = 0
+    while num < n:
+        yield num
+        num += 1
+
+
 def generate_squares(start, end):
     for x in range(start, end, 2):
-        yield f'Square of {x} = {x ** 2}'
+        yield x
+        # yield f'Square of {x} = {x ** 2}'
 
 
 def generate_from(start, end):
     yield from generate_squares(start, end)
 
 
-for square in generate_from(4, 16):
-    print(square)
+def generate_negate(seq):
+    for el in seq:
+        yield -el
+
+
+# A little example of chaining generators
+for negate_square in generate_negate(generate_squares(4, 22)):
+    print(negate_square)
+
+# for square in generate_from(4, 16):
+#     print(square)

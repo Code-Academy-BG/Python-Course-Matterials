ccc1d256cb6f3e008eaa66d35c0c5284753a37c2
Petar Netev <pesho.netev@gmail.com>
2022-03-15 16:55:16 +0200

    Add exersice version

--- functions/exersice.py
+++ functions/exersice.py
@@ -13,128 +13,220 @@ import csv
 import os.path
 import random
 
-from settings.testing import INPUT_DIR
+import os
+from pathlib import Path
 
+BASE_DIR = Path(os.path.abspath(__file__)).parent.parent
+
+INPUT_DIR = os.path.join(BASE_DIR, "test_files", "input")
+
+
+class BadHeaderRow(ValueError):
+    pass
+
+
+HEADERS = {
+    "math": ["student", "exam", "points"],
+}
+
+SCORES = {
+    "math": {
+        "initial_scores": [30, 40, 50],
+        "retake_scores": [20, 30, 40, 50, 60],
+    }
+}
 
 SCORE_TO_PASS = 80
 
-
-# The filename should be changeable as well.
-with open(os.path.join(INPUT_DIR, "students_exams.csv"), "w", newline="") as source:
-    writer = csv.writer(source)
-
-    writer.writerow(
-        [
-            "student",
-            "exam",
-            "points",
-        ]
-    )
-
-    # exam and student counts should be changeable
-    for e in range(1, 11):
-        for s in range(1, 281):
-            writer.writerow(
-                [
-                    f"Student {s}",
-                    e,
-                    # Points should be also manageable
-                    random.randint(random.choice([30, 40, 50]), 100),
-                ],
-            )
-
-
-with open(os.path.join(INPUT_DIR, "students_exams.csv"), "r") as source:
-    reader = csv.reader(source)
-    students_data = {}
-
-    # Add validation against the header row of the file.
-    # This code should be organized as a function to allow process any file.
-
-    for row in reader:
-        if row[0] == "student":
-            continue
-        student_data = students_data.setdefault(
-            row[0],
-            {
-                "student": row[0],
-                "exams": {},
-                "avg": 0,
-            },
-        )
-        student_data["exams"][row[1]] = int(row[2])
-
-
-for student, data in students_data.items():
-    data["avg"] = sum(data["exams"].values()) // len(data["exams"])
-
-# After having the knowledge how many passed and how many failed, please notify the headmaster
-# of the facultate. The headmaster could be changed, so take care of this.
-
-students_to_retake_exams = {
-    student: {
-        "should_retake": data["avg"] < SCORE_TO_PASS,
-        "avg": data["avg"],
-        "failed_exams": [
-            exam for exam in data["exams"] if data["exams"][exam] <= SCORE_TO_PASS
-        ],
-    }
-    for student, data in students_data.items()
+COLORS = {
+    "success": "\033[92m",
+    "fail": "\033[91m",
 }
 
-students_passed = {}
-failed_students = {}
-for student, data in students_to_retake_exams.items():
-    if not data["should_retake"]:
-        students_passed[student] = students_data[student]
-        continue
 
-    while students_data[student]["avg"] < SCORE_TO_PASS:
-        for failed_exam in data["failed_exams"]:
-            for r_i in range(3):
-                # Points should be also manageable
-                retake_score = random.randint(random.choice([20, 30, 40, 50, 60]), 100)
-                if students_data[student]["exams"][failed_exam] >= retake_score:
-                    continue
-                students_data[student]["exams"][failed_exam] = retake_score
-                students_data[student]["avg"] = sum(
-                    students_data[student]["exams"].values(),
-                ) // len(students_data[student]["exams"])
-                if students_data[student]["avg"] >= SCORE_TO_PASS:
-                    students_passed[student] = students_data[student]
-                    break
-                if retake_score >= SCORE_TO_PASS:
-                    break
-            if students_data[student]["avg"] >= SCORE_TO_PASS:
-                break
-        if students_data[student]["avg"] < SCORE_TO_PASS:
-            failed_students[student] = students_data[student]
+def print_bold(*message):
+    print("\033[1m", end="")
+    print(*message)
+    print("\033[21m", end="")
+
+
+def print_message(color, *message):
+    print(COLORS[color], end="")
+    print(*message)
+    print("\033[0m", end="")
+
+
+def _get_file_name(discipline: str) -> str:
+    return f"students_{discipline}_exams.csv"
+
+
+def generate_initial_session_data(
+    discipline: str,
+    filename: str,
+    students_count: int,
+    exams_count: int,
+):
+    """
+    Generate students session data for given discipline, student and exams count.
+    :param discipline:
+    :param filename:
+    :param students_count:
+    :param exams_count:
+    :return: None, store .csv data in INPUT directory
+    """
+
+    with open(os.path.join(INPUT_DIR, filename), "w", newline="") as source:
+        writer = csv.writer(source)
+        writer.writerow(
+            [
+                "student",
+                "exam",
+                "points",
+            ],
+        )
+
+        for e in range(1, exams_count + 1):
+            for s in range(1, students_count + 1):
+                writer.writerow(
+                    [
+                        f"Student {s}",
+                        e,
+                        # Points should be also manageable
+                        random.randint(
+                            random.choice(SCORES[discipline]["initial_scores"]), 100
+                        ),
+                    ],
+                )
+
+
+def read_session_data(filename: str, discipline: str) -> dict:
+    with open(os.path.join(INPUT_DIR, filename), "r") as source:
+        reader = csv.reader(source)
+        students_data = {}
+
+        header_row = next(reader)
+        if header_row != HEADERS[discipline]:
+            raise BadHeaderRow(
+                f"Header row not matching the expected for {discipline}."
+                f" {header_row} != {HEADERS[discipline]}."
+            )
+
+        for student, exam, points in reader:
+            if student == "student":
+                continue
+            student_data = students_data.setdefault(
+                student,
+                {
+                    "student": student,
+                    "exams": {},
+                    "avg": 0,
+                },
+            )
+            student_data["exams"][exam] = int(points)
+    return students_data
+
+
+def update_avg(students_data: dict):
+    for student, data in students_data.items():
+        data["avg"] = sum(data["exams"].values()) // len(data["exams"])
+
+
+def notify_headmaster_about_retakes_count(students_data: dict):
+    retakes = sum(
+        1 for student in students_data.values() if student["avg"] < SCORE_TO_PASS
+    )
+    print_bold(f"Headmaster, we have {retakes} students that need to retake exams")
+    print("\n" * 3)
+
+
+def _retake(data, exam, students_passed, retakes_data, discipline):
+    for r_i in range(3):
+        retake_score = random.randint(
+            random.choice(SCORES[discipline]["initial_scores"]), 100
+        )
+        if data["exams"][exam] >= retake_score:
+            continue
+        student_retakes_data = retakes_data.setdefault(data["student"], {})
+        exam_data = student_retakes_data.setdefault(exam, {"before": [], "after": []})
+        exam_data["before"].append(data["exams"][exam])
+        exam_data["after"].append(retake_score)
+        data["exams"][exam] = retake_score
+        data["avg"] = sum(data["exams"].values()) // len(data["exams"])
+        if data["avg"] >= SCORE_TO_PASS:
+            students_passed[data["student"]] = data
+            break
+        if retake_score >= SCORE_TO_PASS:
             break
 
 
-print(len(students_passed) + len(failed_students))
-assert (
-    len(students_passed) + len(failed_students) == 280
-)  # This is extremely magic, should be handled
+def handle_retakes(students_data, discipline):
+    print_bold("=" * 10, "START retakes", "=" * 10)
+    students_passed = {}
+    failed_students = {}
+    retakes_data = {}
+    for student, data in students_data.items():
+        print_bold(f"Student {student} start retake!")
+        if data["avg"] >= SCORE_TO_PASS:
+            students_passed[student] = data
+            continue
+
+        while data["avg"] < SCORE_TO_PASS:
+            failed_exams = [
+                e for e in data["exams"] if data["exams"][e] < SCORE_TO_PASS
+            ]
+            for exam in failed_exams:
+                _retake(data, exam, students_passed, retakes_data, discipline)
+                if data["avg"] >= SCORE_TO_PASS:
+                    print_message("success", f"Student {student} passed retake :)")
+                    break
+            if data["avg"] < SCORE_TO_PASS:
+                print_message("fail", f"Student {student} failed retake :(")
+                failed_students[student] = data
+                break
+    print_bold("=" * 10, "END retakes", "=" * 10)
+    return students_passed, failed_students, retakes_data
 
 
-for data in sorted(
-    students_passed.values(),
-    key=lambda item: item["avg"],
-    reverse=True,
-):
-    print(f"Student {data['student']} passed session with score: {data['avg']}")
-    if data["student"] in students_to_retake_exams:
-        print(f"Student {data['student']} passed after participation in retakes.")
-        # Print out scores before retakes
-        # Print out scores after retakes
+def notify_results(students_passed, failed_students, retakes_data):
+    for data in sorted(
+        students_passed.values(),
+        key=lambda item: item["avg"],
+        reverse=True,
+    ):
+        print_message(
+            "success",
+            f"Student {data['student']} passed session with score: {data['avg']}",
+        )
+        if data["student"] in retakes_data:
+            print_message(
+                "success",
+                f"Student {data['student']} passed after participation in retakes.",
+            )
+            for exam, exam_data in retakes_data[data["student"]].items():
+                print(" " * 4, f"Exam {exam} data")
+                for idx, e in enumerate(exam_data["before"]):
+                    print(
+                        " " * 4,
+                        f"exam {exam} before {e}, after: {exam_data['after'][idx]}",
+                    )
 
-# Add some info about % of students who passed the session. Print out the % who failed the session
-print("\n" * 4)
+    print("\n" * 4)
 
-for student, data in failed_students.items():
-    print(f"Student {student} failed session with score: {data['avg']}")
+    for student, data in failed_students.items():
+        print_message(
+            "fail",
+            f"Student {student} failed session with score: {data['avg']}",
+        )
 
 
-def run_session():
-    pass
+def run_session(discipline):
+    generate_initial_session_data(discipline, _get_file_name(discipline), 220, 5)
+    students_data = read_session_data(_get_file_name(discipline), discipline)
+    update_avg(students_data)
+    notify_headmaster_about_retakes_count(students_data)
+    passed, failed, retakes_data = handle_retakes(students_data, discipline)
+    assert len(passed) + len(failed) == len(students_data)
+    notify_results(passed, failed, retakes_data)
+
+
+run_session("math")

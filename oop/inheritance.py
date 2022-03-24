25902ffb251d17bd32f16f74e6353e504a1f49c2
Petar Netev <pesho.netev@gmail.com>
2022-03-24 20:37:22 +0200

    Add EvensOnly custom list

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -1,3 +1,6 @@
+import random
+
+
 class DictOfShortKeys(dict):
     MAX_KEY_LENGTH = 6
 
@@ -17,8 +20,25 @@ short_key_dictionary["Pesho"] = "Good name"
 
 
 class OddsOnly(list):
-    def append(self, item) -> None:
+    def append(self, item: int) -> None:
         if item % 2 != 0:
             super().append(item)
             return
         raise ValueError("Only odd ints are acceptable")
+
+
+class EvensOnly(list):
+    def append(self, item: int) -> None:
+        if item % 2 == 0:
+            super().append(item)
+            return
+        raise ValueError("Only even ints are acceptable")
+
+
+evens_only = EvensOnly()
+while len(evens_only) < 100:
+    random_integer = random.randint(1, 100)
+    try:
+        evens_only.append(random_integer)
+    except ValueError:
+        continue

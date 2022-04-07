86ca342c7cceacffe4404b1b854e4a5fc76c0d20
Petar Netev <pesho.netev@gmail.com>
2022-04-07 17:24:22 +0300

    Add in lecture activity

--- oop/person.py
+++ oop/person.py
@@ -7,6 +7,7 @@ class Person:
         self.height = height
         self.weight = weight
         self.name = name
+        self.relations = set()
 
     def __validate_gender(self, gender):
         if gender not in self.GENDER_OPTIONS:
@@ -19,6 +20,32 @@ class Person:
 
     def __str__(self):
         return f"I am {self.name}"
+
+    def __repr__(self):
+        return f"Person(name={self.name}, age={self.age})"
+
+    def __len__(self):
+        print(self.__dict__)
+        return len(self.__dict__)
+
+    def __int__(self):
+        return self.age
+
+    def __eq__(self, other):
+        comparing_elements = ["age", "height", "weight"]
+        return all(
+            getattr(self, element) == getattr(other, element)
+            for element in comparing_elements
+        )
+
+    def __gt__(self, other):
+        return self.height > other.height
+
+    def __add__(self, other):
+        self.relations.add(other)
+
+    def __sub__(self, other):
+        self.relations.remove(other)
 
     def custom_method(self):
         print("Custom method from Person")
@@ -47,7 +74,11 @@ class Person:
 
 
 person_pesho = Person.create_person("male", 31, 180, 75, "Pesho")
+person_mitko = Person.create_person("male", 31, 180, 75, "Mitko")
 print(person_pesho)
+
+print("Is Mitko equal to Pesho?")
+print("Yes" if person_mitko == person_pesho else "No")
 
 
 class PersonBase:

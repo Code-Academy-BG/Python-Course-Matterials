27f6f45db1c2f06996bd0ab0c2d8449bd2d96f02
Petar Netev <pesho.netev@gmail.com>
2022-04-12 17:15:13 +0300

    Add homework

--- exceptions/homework.py
+++ exceptions/homework.py
@@ -1,19 +1,48 @@
+import os
+
+import requests
+
 from settings.testing import INPUT_DIR
 
-5 / 0
-Decimal("100")
-open(os.path.join(INPUT_DIR, "some_nasty_bug_file.txt"))
+try:
+    5 / 0
+except ZeroDivisionError:
+    print("Not a good practice to delete by 0")
 
-list_a = range(29)
-list_a[5]
+try:
+    Decimal("100")
+except NameError:
+    print("Hm, it seems, there is no such module in my scope?")
 
-dict_b = {"some_key": "some_value", 5: 10, (): False}
-dict_b[True]
+try:
+    open(os.path.join(INPUT_DIR, "some_nasty_bug_file.txt"))
+except FileNotFoundError:
+    print("You're trying to cheat me with some buggy file, don't you!")
 
-list_a.pesho
+try:
+    list_a = range(29)
+    list_a[5]
+except IndexError:
+    print("Please learn the range API before attempt to use it.")
 
-dict_b.codeacademy
+try:
+    dict_b = {"some_key": "some_value", 5: 10, (): False}
+    dict_b[True]
+except KeyError:
+    print("I don't have True value")
 
-print(AmIRealObject)
+try:
+    list_a.pesho
+    dict_b.codeacademy
+except AttributeError:
+    print("What are you trying to achieve?")
 
-response = requests.post("http://alabala.koisiti")
+try:
+    print(AmIRealObject)
+except NameError:
+    print("AmiRealObject is not a real object, or at least I don't have it inside my scope")
+
+try:
+    response = requests.post("http://alabala.koisiti")
+except requests.ConnectionError:
+    print("alabala.koisiti is not available for you")

e4ac8d2f89cd32e67d0725a5cdb24a62fdfecb3d
Petar Netev <pesho.netev@gmail.com>
2022-04-19 20:31:07 +0300

    Add MergeRequest materials

--- testing/merge_request/merge_request.py
+++ testing/merge_request/merge_request.py
@@ -1,10 +1,16 @@
 from enum import Enum
 
 
+class MergeRequestException(Exception):
+    pass
+
+
 class MergeRequestStatus(Enum):
     APPROVED = "approved"
     REJECTED = "rejected"
     PENDING = "pending"
+    OPEN = "open"
+    CLOSED = "closed"
 
 
 class MergeRequest:
@@ -13,6 +19,14 @@ class MergeRequest:
             "upvotes": set(),
             "downvotes": set(),
         }
+        self._status = MergeRequestStatus.OPEN
+
+    def close(self):
+        self._status = MergeRequestStatus.CLOSED
+
+    def _cannot_vote_if_closed(self):
+        if self._status == MergeRequestStatus.CLOSED:
+            raise MergeRequestException("can't vote on closed merge request")
 
     @property
     def status(self):
@@ -22,11 +36,20 @@ class MergeRequest:
         In other cases status should be Pending
         :return:
         """
+        from testing.merge_request.acceptance_threshold import AcceptanceThreshold
 
-        return
+        if self._status == MergeRequestStatus.CLOSED:
+            return self._status
+        return AcceptanceThreshold(self._context).status()
 
     def upvote(self, by_user):
-        pass
+        self._cannot_vote_if_closed()
+
+        self._context["downvotes"].discard(by_user)
+        self._context["upvotes"].add(by_user)
 
     def downvote(self, by_user):
-        pass
+        self._cannot_vote_if_closed()
+
+        self._context["downvotes"].add(by_user)
+        self._context["upvotes"].discard(by_user)
--- testing/merge_request/tests/test_unittest.py
+++
@@ -1,5 +0,0 @@
-from unittest import TestCase
-
-
-class TestMergeRequestStatus(TestCase):
-    pass
